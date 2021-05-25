79fd73733e818ce5e676e371e427d6ec02fcd8ec
nlee097 <nlee097@ucr.edu>
2021-05-25 06:51:02 -0700

    added integration tests

--- ENathanLe_tests.hpp
+++ ENathanLe_tests.hpp
@@ -51,4 +51,33 @@ TEST(ENathanLeTest, NegMultSubEval) {
 	delete sub;
 }
 
+TEST(IntegrateVisitorTests, commonCase) {
+	Base* pos = new Op(4);
+	Base* neg = new Op(-1);
+	Base* two = new Op(2);
+	Base* add = new Add(pos, neg);
+	Base* pow = new Pow(add, two);
+	std::string str = "";
+	Visitor* v = new LatexVisitor();
+	for(Iterator it(pow); !it.is_done(); it.next()){
+	    str += it.current_node().accept(v, it.current_index());
+	}
+	EXPECT_EQ("$" + str + "$", "${({({4}+{-1}^{2})})}$");
+}
+
+TEST(IntegrateVisitorTests, multDiv) {
+        Base* four = new Op(4);
+        Base* three = new Op(3);
+        Base* two = new Op(2);
+        Base* mult = new Mult(four, three);
+        Base* div = new Div(mult, two);
+        std::string str = "";
+        Visitor* v = new LatexVisitor();
+        for(Iterator it(div); !it.is_done(); it.next()){
+            str += it.current_node().accept(v, it.current_index());
+        }
+        EXPECT_EQ("$" + str + "$", "${\frac{({4}\cdot{3})}{2})}$");
+}
+
+
 #endif
